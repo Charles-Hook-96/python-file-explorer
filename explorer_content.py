935cb9ac0bafab62d093bf747b7f9af8224c9a07
Charles Hook <79293041+Charles-Hook-96@users.noreply.github.com>
2026-01-16 16:01:25 -0500

    adding paned window

---
+++ explorer_content.py
@@ -0,0 +1,4 @@
+import pathlib
+
+def get_file_path() -> pathlib.Path:
+    return pathlib.Path.cwd()
--- frame.py
+++ frame.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 from tkinter import TclError, ttk, filedialog
+from explorer_content import *
 
-def build_root_window():
+def build_root_window() -> tk.Tk:
     root = tk.Tk()
     root.configure(background="grey")
 
@@ -18,11 +19,31 @@ def build_root_window():
 
     return root
 
-def build_inner_frame(root):
+def build_inner_frame(root) -> tk.Frame:
     inner_frame = ttk.Frame(root, padding="15", relief="sunken")
     inner_frame.pack(fill='both', expand=True, padx=15, pady=15)
+    return inner_frame
+
+def build_explorer_frame(inner_frame):
+    paned = ttk.Panedwindow(inner_frame, orient='horizontal')
+    paned.pack(fill='both', expand=True)
+
+    left_frame = ttk.Frame(paned, relief='sunken', borderwidth=1)
+    paned.add(left_frame, weight=1)
+
+    right_frame = ttk.Frame(paned, relief='sunken', borderwidth=1)
+    paned.add(right_frame, weight=3)
+
+    tree = ttk.Treeview(left_frame)
+    tree.pack(fill='both', expand=True)
+
+    tree.insert('', 'end', text=str(get_file_path()))
+    tree.insert('', 'end', text='Folder 2')
+
+
 
 def build_explorer():
     root = build_root_window()
-    build_inner_frame(root)
+    inner_frame = build_inner_frame(root)
+    build_explorer_frame(inner_frame)
     root.mainloop()
